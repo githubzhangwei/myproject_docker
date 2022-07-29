9c69f847032f236c236395bbeaf32238f343a94e
zhangwei <2401282499@qq.com>
2022-07-29 15:55:08 +0800

    400 - uwsgi 配置导致的 nginx 访问

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#-l0@*&f&eaxteqqctn)57-01b-4w(1--c6niy_)3f@=srfuv5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
